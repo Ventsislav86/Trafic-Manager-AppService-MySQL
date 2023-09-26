d0fab0d50401ed9f11cb757333b3dc9ba04677f0
Ventsislav86 <ventsislav_rs@abv.bg>
2023-09-26 18:02:11 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,10 +9,10 @@ terraform {
 
 
 provider "azurerm" {
-  subscription_id = "7f9039ab-1882-420d-9790-518f14352619"
-  tenant_id       = "93f33571-550f-43cf-b09f-cd331338d086"
-  client_id       = "dd0e81bd-a4a1-4aba-a532-6d8c4c9beccf"
-  client_secret   = "VhJ8Q~nCQ8z7kaXdDvNVVWb1hdxJHEljdCyGfb.K"
+  subscription_id = ""
+  tenant_id       = ""
+  client_id       = ""
+  client_secret   = ""
   features {
     resource_group {
     }
